8190b083084e82b38ca24f5f0bf987d50758e497
Annehz <luckylzn@gmail.com>
2025-04-03 18:00:05 +0800

    completed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # synthetic_data_generation_tabular_experiments
+
+
+'''bash
+ python main.py --folder_path real_data 
+    --path_to_metadata metadata_fake_hotel_guests.json 
+    --save_path synthetic_data_sdv.csv
+'''
+
+'''bash
+ python main.py --folder_path real_data 
+    --save_path synthetic_data_sdv.csv
+'''
--- utilities.py
+++ utilities.py
@@ -1,10 +1,15 @@
+from typing import Literal
 import pandas as pd
 from sdv.datasets.local import load_csvs
 from sdv.metadata import Metadata
-from sdv.multi_table import HMASynthesizer
-from sdv.single_table import GaussianCopulaSynthesizer, CTGANSynthesizer, TVAESynthesizer, CopulaGANSynthesizer
+from sdv.single_table import (
+    GaussianCopulaSynthesizer,
+    CTGANSynthesizer,
+    TVAESynthesizer,
+    CopulaGANSynthesizer,
+)
 from sdv.evaluation.single_table import evaluate_quality
-# from sdv.tabular import CTGAN  # or GaussianCopula, CopulaGAN, TVAE
+
 
 def read_csv_data(folder: str) -> dict[pd.DataFrame]:
     data = load_csvs(folder_name=folder)
@@ -13,47 +18,67 @@ def read_csv_data(folder: str) -> dict[pd.DataFrame]:
     # print(f"Number of tables: {count}")
     return data, count
 
+
 def autodetect_metadata(data: pd.DataFrame) -> dict:
     metadata = Metadata.detect_from_dataframes(data)
     return metadata
 
-def select_model(metadata:dict, model_type: str='') -> callable:
-    if model_type == 'GaussianCopula':
+
+def select_model(
+    metadata: dict,
+    model_type: str = Literal["GaussianCopula", "CTGAN", "CopulaGAN", "TVAE"],
+) -> callable:
+
+    if model_type == "GaussianCopula":
         return GaussianCopulaSynthesizer(
+            metadata, enforce_min_max_values=True, enforce_rounding=True
+        )
+    elif model_type == "CTGAN":
+        return CTGANSynthesizer(
+            metadata, enforce_rounding=True, epochs=500, verbose=True
+        )
+    elif model_type == "CopulaGAN":
+        return CopulaGANSynthesizer(
             metadata,
-            enforce_rounding=False,
+            enforce_min_max_values=True,
+            enforce_rounding=True,
             epochs=500,
             verbose=True,
         )
-    elif model_type == 'CTGAN':
-        return CTGANSynthesizer
-    elif model_type == 'CopulaGAN':
-        return CopulaGANSynthesizer
-    elif model_type == 'TVAE':
-        return TVAESynthesizer
+    elif model_type == "TVAE":
+        return TVAESynthesizer(
+            metadata, enforce_min_max_values=True, enforce_rounding=True, epochs=500
+        )
     else:
-        raise ValueError("Invalid model type. Choose from 'CTGAN', 'GaussianCopula', 'CopulaGAN', or 'TVAE'.")
+        raise ValueError(
+            "Invalid model type. Choose from 'CTGAN', 'GaussianCopula', 'CopulaGAN', or 'TVAE'."
+        )
 
-def create_synthetic_data(metadata, data, model_type='GaussianCopulaSynthesizer', num_rows=500):
-    # Choose the model
-    model_class = {
-        'CTGAN': CTGANSynthesizer(metadata),
-        'GaussianCopula': GaussianCopulaSynthesizer(metadata),
-        'CopulaGAN': CopulaGANSynthesizer(metadata),
-        'TVAE': TVAESynthesizer(metadata),
-    }.get(model_type, GaussianCopulaSynthesizer(metadata))
 
-    model = model_class()
-    model.fit(data)
-    synthetic_data = model.sample(num_rows)
-    # synthesizer = GaussianCopulaSynthesizer(metadata)
-    # synthesizer.fit(data=data)
-    # synthetic_data = synthesizer.sample(num_rows)
+def create_synthetic_data(
+    metadata,
+    data,
+    single_table_name,
+    model_type=Literal["GaussianCopula", "CTGAN", "CopulaGAN", "TVAE"],
+    num_rows=500,
+):
+    synthesizer = select_model(metadata, model_type)
+    synthesizer.fit(data=data[single_table_name])
+    synthetic_data = synthesizer.sample(num_rows)
     return synthetic_data
 
-def evaluate_synthetic_data(synthetic_data, real_data, metadata, single_table_name):
+
+def evaluate_synthetic_data(
+    synthetic_data,
+    real_data,
+    metadata,
+    single_table_name,
+) -> tuple:
     quality_report = evaluate_quality(
-        real_data[single_table_name],
-        synthetic_data,
-        metadata)
-    return quality_report
+        real_data[single_table_name], synthetic_data, metadata
+    )
+    return (
+        quality_report,
+        quality_report.get_details(property_name="Column Shapes")["Score"].mean(),
+        quality_report.get_details(property_name="Column Pair Trends")["Score"].mean(),
+    )
